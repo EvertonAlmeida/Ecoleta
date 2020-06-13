6fcc133a4cebfe6bfb303e13a12b549209cbc7f5
Everton Almeida <everton.o.almeida@outlook.com>
2020-06-13 19:53:46 +0100

    Items list added

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hello World'});
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select("*");
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://192.168.0.45:3333/uploads/${item.image}`,
+    };
+  });
+
+  response.json(serializedItems);
 })
 
 export default routes;
