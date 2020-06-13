6a034abb26c6db7b6c6c6c9e814ad093d95021f0
Everton Almeida <everton.o.almeida@outlook.com>
2020-06-13 18:42:16 +0100

    Connection with pg and created migrations

---
+++ backend/src/database/migrations/20200613174854_Create_points.ts
@@ -0,0 +1,22 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('points', table => {
+    table.increments('id').primary();
+    table.string('image').notNullable();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.decimal('latitude',20,12).notNullable();
+    table.decimal('longitude',20,12).notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('points');
+}
+
+
