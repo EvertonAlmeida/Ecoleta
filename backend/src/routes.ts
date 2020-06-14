2f0f3632168d07044490f3accf698a07a99e80c3
Everton Almeida <everton.o.almeida@outlook.com>
2020-06-14 11:00:56 +0100

    Added post of Points

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,4 +17,53 @@ routes.get('/items', async (request, response) => {
   response.json(serializedItems);
 })
 
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const point = {
+    image: 'fake-image',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  }
+
+  const insertedIds = await trx('points').returning('id').insert(point);
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items
+    .split(',')
+    .map((item: string) => Number(item.trim()))
+    .map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    }
+  });
+
+ await trx('point_items').insert(pointItems)
+
+ await trx.commit();
+
+  return response.json({ 
+    id: point_id,
+    ...point
+  });
+})
+
 export default routes;
